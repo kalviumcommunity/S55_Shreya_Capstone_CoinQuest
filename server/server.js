9940609309a0e47dde878c05f92de62fbd80e1f2
shhhreya645 <shreya.pawar@kalvium.community>
2024-07-29 16:34:04 +0530

    get-api used

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const router = require('./Routes'); 
+const routes = require('./Routes'); 
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 let status = "disconnected";
 
-app.use(cors());
-app.use(express.json()); 
-
 dotenv.config();
 const secretKey = process.env.SECRET_KEY;
 
 // Debugging: Verify that the MONGO environment variable is loaded
-console.log(process.env.MONGO);
+console.log(`MONGO: ${process.env.MONGO}`);
 
 const startConnect = async () => {
-  console.log(process.env.MONGO)
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     status = "connected"; 
     console.log("Connected to MongoDB");
   } catch (err) {
@@ -30,19 +29,27 @@ const startConnect = async () => {
 };
 
 const stopConnect = async () => {
-  await mongoose.disconnect();
-  status = "disconnected"; 
-  console.log("Disconnected from MongoDB");
+  try {
+    await mongoose.disconnect();
+    status = "disconnected"; 
+    console.log("Disconnected from MongoDB");
+  } catch (err) {
+    console.error("Failed to disconnect from MongoDB:", err);
+  }
 };
 
-app.use(router);
+
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send(status);
 });
 
 startConnect(); 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
